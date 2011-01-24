4f633042710c4a25c3bdd16c2888c91742a47ce0
Jerome Quelin <jquelin@cpan.org>
2011-01-24 14:34:29 +0000

    Import of JQUELIN/App-Magpie-1.110240 from CPAN.
    gitpan-cpan-distribution: App-Magpie
    gitpan-cpan-version:      1.110240
    gitpan-cpan-path:         JQUELIN/App-Magpie-1.110240.tar.gz
    gitpan-cpan-author:       JQUELIN
    gitpan-cpan-maturity:     released

---
+++ lib/App/Magpie/App/Command.pm
@@ -0,0 +1,61 @@
+#
+# This file is part of App-Magpie
+#
+# This software is copyright (c) 2011 by Jerome Quelin.
+#
+# This is free software; you can redistribute it and/or modify it under
+# the same terms as the Perl 5 programming language system itself.
+#
+use 5.012;
+use strict;
+use warnings;
+
+package App::Magpie::App::Command;
+BEGIN {
+  $App::Magpie::App::Command::VERSION = '1.110240';
+}
+# ABSTRACT: base class for sub-commands
+
+use App::Cmd::Setup -command;
+
+# -- public methods
+
+
+1;
+
+
+=pod
+
+=head1 NAME
+
+App::Magpie::App::Command - base class for sub-commands
+
+=head1 VERSION
+
+version 1.110240
+
+=head1 DESCRIPTION
+
+This module is the base class for all sub-commands. It doesn't do
+anything special currently but trusting methods for pod coverage.
+
+=for Pod::Coverage::TrustPod description
+    opt_spec
+    execute
+
+=head1 AUTHOR
+
+  Jerome Quelin <jquelin@gmail.com>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2011 by Jerome Quelin.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
+
+
+__END__
+
---
+++ lib/App/Magpie/App/Command/bswait.pm
@@ -0,0 +1,97 @@
+#
+# This file is part of App-Magpie
+#
+# This software is copyright (c) 2011 by Jerome Quelin.
+#
+# This is free software; you can redistribute it and/or modify it under
+# the same terms as the Perl 5 programming language system itself.
+#
+use 5.012;
+use strict;
+use warnings;
+
+package App::Magpie::App::Command::bswait;
+BEGIN {
+  $App::Magpie::App::Command::bswait::VERSION = '1.110240';
+}
+# ABSTRACT: pause according to build-system recommendations
+
+use LWP::UserAgent;
+
+use App::Magpie::App -command;
+
+
+# -- public methods
+
+sub description {
+"Pauses according to the recommendation of Mageia build-system.
+Build-system provides some recommendation on how much time to pause
+between 2 packages submission to not overload it - this is known as
+throttling."
+}
+
+sub opt_spec {
+    my $self = shift;
+    return (
+        [],
+        [ 'display|d!' => 'only display time to pause' ],
+        [ 'sleep|s!'   => 'sleep accordingly (default, --nosleep to negate)' ],
+    );
+}
+
+sub execute {
+    my ($self, $opts, $args) = @_;
+
+    $opts->{sleep} //= 1;
+
+    my $ua = LWP::UserAgent->new;
+    $ua->timeout(10);
+    $ua->env_proxy;
+
+    my $response = $ua->head('http://pkgsubmit.mageia.org/');
+    die $response->status_line unless $response->is_success;
+
+    my $sleep = $response->header( "x-bs-throttle" )+10;
+
+    say $sleep    if $opts->{display};
+    sleep($sleep) if $opts->{sleep};
+}
+
+1;
+
+
+=pod
+
+=head1 NAME
+
+App::Magpie::App::Command::bswait - pause according to build-system recommendations
+
+=head1 VERSION
+
+version 1.110240
+
+=head1 DESCRIPTION
+
+This command pauses according to the recommendation of Mageia
+build-system. Indeed, instead of pushing all your packages to be
+rebuilt, it's better to throttle them one at a time. Build-system
+provides some recommendation on how much to pause between 2 packages -
+and this command uses this hint to pause accordingly.
+
+=head1 AUTHOR
+
+  Jerome Quelin <jquelin@gmail.com>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2011 by Jerome Quelin.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
+
+
+__END__
+
+
